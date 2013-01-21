802db81c3abe9e11223a966d549e8df9f5576ca6
David <david@addepar.com>
2013-01-21 13:59:18 -0800

    cleaned up the code a bit

--- python/Problem5.py
+++ python/Problem5.py
@@ -9,16 +9,16 @@ __author__ = 'Winton'
 #Because we are looking for the minimal number, we take the number which satisfies the above
 #condition and has no other factors.
 
-#map(prime_factorization, xrange(1,21)) ->
 # list of prime factorization maps of the numbers from 1 to 20
+prime_factor_maps = map(prime_factorization, xrange(1, 21))
 
 #reduce(dictionary_union, map(prime_factorization, xrange(1, 21)), {}) ->
 # takes that list and pairwise finds the maximum multiplicity for each prime factor. this is the
 # prime factorization of our answer
+union = reduce(factorization_union, prime_factor_maps, {})
 
 #takes the map of maximal multiplicities and raises each factor to the associated power
+flat_factors = [x ** v for x, v in union.iteritems()]
 
 #multiplies the result together to get our answer
-
-
-print reduce(operator.mul, [x ** v for x,v in reduce(factorization_union, map(prime_factorization, xrange(1,21)), {}).iteritems()], 1)
+print reduce(operator.mul, flat_factors, 1)
